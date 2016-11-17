cac7e44564d77165753dca296b6f2be5721de60c
Filip Kucera <kucerafilip@gmail.com>
2016-11-17 17:13:29 +0100

    lombock

---
+++ rest-server/src/main/java/cz/fku/logic/playground/LombockTest.java
@@ -0,0 +1,14 @@
+package cz.fku.logic.playground;
+
+/**
+ * Created by Filip on 17.11.2016.
+ */
+
+import lombok.Data;
+
+@Data
+public class LombockTest {
+    String field1;
+    final int field2;
+
+}
